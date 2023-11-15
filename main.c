5b81521863871e88f50d2949a589d894b4241fcb
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-11-16 01:15:40 +0200

    hot

--- main.c
+++ main.c
@@ -1,175 +1,95 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/**
- * search_command - searches for command.
- *
- * @args: inputs args.
- * @env: inputs env.
- *
- * Return: 0 if found, -1 if not found
-*/
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
 
-int search_command(char *args[], char **env)
+size_t custom_strcspn(const char *str, const char *delim)
 {
-	char *temp = NULL, *token = NULL, *path = _getenv("PATH", env);
-	struct stat st;
-
-	token = strtok(path, ":");
-	temp = str_concat(token, args[0]);
-	while (stat(temp, &st) != 0)
-	{
-		free(temp);
-		token = strtok(NULL, ":");
-		if (token == NULL)
-		{
-			free(path);
-			return (-1);
-		}
-		temp = str_concat(token, args[0]);
-	}
-
-	args[0] = temp;
-	free(path);
-	return (0);
+    /* ... (custom_strcspn implementation remains unchanged) */
 }
 
-/**
- * excute - excutes command.
- *
- * @args: inputs arguments.
- * @env: inputs env.
- * @argv: inputs argv.
- * @line: inputs line ptr.
- *
- * Return: 0 on success, -1 on failure.
-*/
-
-void excute(char *args[], char **env, char **argv, int *line)
+void execute_command(char *command)
 {
-	pid_t child;
-	int status, temp, flag = 0;
-	struct stat st;
+    char *args[MAX_ARGUMENTS];
+    int arg_count = 0;
 
-	temp = stat(args[0], &st);
-	if (temp != 0)
-	{
-		temp = search_command(args, env);
-		flag = 1;
-	}
-	if (temp != 0)
-	{
-		printf("%s: %i: fsd: not found\n", argv[0], *line);
-		return;
-	}
-	else
-	{
-		child = fork();
-		if (child != 0)
-		{
-			wait(&status);
-			if (flag)
-				free(args[0]);
-			return;
-		}
-		execve(args[0], args, env);
-	}
+    char *token = strtok(command, " ");
+    while (token != NULL && arg_count < MAX_ARGUMENTS - 1)
+    {
+        args[arg_count++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL;
+
+    if (strcmp(args[0], "exit") == 0)
+    {
+        printf("Exiting shell...\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("Fork error");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            printf("Command '%s' not found\n", args[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        int status;
+        waitpid(pid, &status, 0);
+    }
 }
 
-
-/**
- * parser - parses the input
- *
- * @args: inputs args array.
- * @buf: inputs buffer of input.
-*/
-
-void parser(char *args[], char *buf)
+void read_and_execute()
 {
-	int i;
-	char *token;
+    char *command = NULL;
+    size_t bufsize = 0;
 
-	i = 0;
-	token = strtok(buf, " \t\n");
-	while (token != NULL && i < MAX_ARG - 1)
-	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " \t\n");
-	}
+    while (1)
+    {
+        printf("$ ");
 
-	args[i] = NULL;
+        if (getline(&command, &bufsize, stdin) == -1)
+        {
+            if (feof(stdin))
+            {
+                printf("\nEnd of file (Ctrl+D) detected. Exiting...\n");
+                break;
+            }
+            else
+            {
+                perror("Error reading command");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        command[custom_strcspn(command, "\n")] = '\0';
+
+        if (strlen(command) > 0)
+        {
+            execute_command(command);
+        }
+    }
+
+    free(command);
 }
 
-/**
- * interface - interface for the shell.
- *
- * @buf: inputs buffer.
- * @line: inputs line ptr.
- *
- * Return: returns characters read on success, -1 of fail or eof.
-*/
-
-ssize_t interface(char **buf, int *line)
+int main()
 {
-	ssize_t temp = 0;
-	size_t n = 0;
-
-	(*line)++;
-	if (isatty(STDIN_FILENO))
-		write(1, "$ ", 2);
-
-	temp = getline(buf, &n, stdin);
-
-	if (_strncmp(*buf, "exit", 4) == 0)
-		return (-1);
-
-	if (_strncmp(*buf, "env", 3) == 0)
-		return (-2);
-
-	return (temp);
+    read_and_execute();
+    return 0;
 }
 
-/**
- * main - entry point
- *
- * @argc: argc.
- * @argv: argv.
- * @env: env.
- *
- * Return: 0
-*/
-
-
-int main(int argc, char **argv, char **env)
-{
-	char *buf = NULL;
-	char *args[MAX_ARG];
-	int temp;
-	int line = 0;
-
-	signal(SIGINT, void (*)(int,  char *)NThandler);
-	while (1)
-	{
-		temp = interface(&buf, &line);
-
-		if (temp == -1)
-		{
-			free(buf);
-			break;
-		}
-		if (temp == -2)
-		{
-			print_env(env);
-			free(buf);
-			continue;
-		}
-		parser(args, buf);
-
-		excute(args, env, argv, &line);
-
-		free(buf);
-	}
-
-	(void)argc;
-	return (0);
-}
